7c560e0355853cbe6cf6385c630ffc2a18427d49
divi1127 <divyadharshini1109@gmail.com>
2026-06-15 14:14:13 +0530

    db commit

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "http://localhost:3000",
+   
     "https://crm-frontend-blond-phi.vercel.app"
   ],
   credentials: true
